072604d3b654d4199357806817d36e63acd59179
mm2201 <marcelo2201maia@gmail.com>
2019-11-12 09:04:14 -0300

    Initial commit

---
+++ app/src/main/java/com/example/diabetecontrol/SegundaTela.java
@@ -0,0 +1,14 @@
+package com.example.diabetecontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SegundaTela extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda_tela);
+    }
+}
